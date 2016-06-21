835fa0664551ebfa837e3852a09e95c3dacd5b7d
Sam <sammakman@gmail.com>
2016-06-21 16:00:24 -0600

    adding unit tests:

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,7 @@ class ApiController < ApplicationController
  # get
  # /api/lookup
  # params device_id
+ #I believe this is depriciated. Should be removed.
   def lookup
     @user = User.find_by( device_id: params[:device_id])
     if @user
@@ -18,12 +19,12 @@ class ApiController < ApplicationController
   # params device_id
   # returns unique_id
   def unregistered
-    @unregistered = UnregisteredDevice.create(device_id: params[:device_id] , unique_id: generate_code(10) )
+    @unregistered = UnregisteredDevice.create(device_id: params[:device_id] , unique_id: generate_code(10))
     if @unregistered
       render :json => {registration_id: @unregistered[:unique_id],
                         message: "Device is not Registered"}
     else
-       render :json => {error: "ID does not exist"}
+       render :json => {status: "500", error: "ID does not exist"}
     end
   end
 
