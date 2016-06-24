b197e03444defa9bc047aa78701b33c2749443cb
Tyler Moody <tmoody@lclark.edu>
2016-06-24 11:02:22 -0700

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONENUMBER_REGEX = ^[2-9]\d{2}-\d{3}-\d{4}$
   validates :email, presence: true, length: { maximum: 255 },
           format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :home_phone, format: { with:VALID_PHONENUMBER_REGEX }
+  validates :cell_phone, format: { with:VALID_PHONENUMBER_REGEX }
 end
