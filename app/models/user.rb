2a3e1a3afb9ad17e8804720b4cbcee38079e403a
Tyler Moody <tmoody@lclark.edu>
2016-06-24 11:10:54 -0700

    fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PHONENUMBER_REGEX = ^[2-9]\d{2}-\d{3}-\d{4}$
+  VALID_PHONENUMBER_REGEX = /\A[2-9]\d{2}-\d{3}-\d{4}\z/i
   validates :email, presence: true, length: { maximum: 255 },
           format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :first_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160624174427) do
 
   create_table "caregivers", force: :cascade do |t|
