d92a3ba7d150d2e4f28aebe9079d667d046fabcc
Tyler Moody <tmoody@lclark.edu>
2016-06-27 15:39:00 -0700

    bleh commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
 root "users#new"
 
 get 'api/lookup'
@@ -8,6 +10,10 @@ get 'api/unregistered'
 get 'users/new'
 post'users', to: "users#create"
 get 'api/regcode'
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 
 get 'caregivers', to: 'caregivers#new'
 post 'caregivers', to: 'caregivers#create'
