a6df19cd7349ad3889ec64902856ebf70a010985
Sam <sammakman@gmail.com>
2016-06-27 21:18:43 -0700

    fixed user account creation validation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
 def index
-
 end
 
 def new
@@ -9,43 +8,28 @@ def new
 end
 
 def create
-
-#   if params[:unique_id].nil?
-#     flash[:error] = "please enter a unique id"
-#     redirect_to root_path
-#
-# else
   @device = UnregisteredDevice.find_by(unique_id: params[:unique_id].downcase)
   if @device && @device[:active]
     flash[:error] = "This Device already has been registered"
-    redirect_to root_path
+    render 'new'
   else
-    @unreg = UnregisteredDevice.find_by(unique_id: params[:unique_id].downcase)
-    if @unreg
-      @user =  User.new(email: params[:email], device_id: @unreg[:device_id])
+    if @device
+      @user = User.new(reg_params)
+      @user[:device_id] = @device[:device_id]
       if @user.save
-          @unreg.update_column(:active, true)
+          @device.update_column(:active, true)
           redirect_to caregivers_path
-        render 'success'
       else
-        if User.find_by( email: params[:email] )
-          flash[:error] = "Email Already Registered"
-        else
-        flash[:error] = "Invalid Email"
-        end
-        redirect_to root_path
+        render 'new'
       end
-    else
-      flash[:error] = "Invalid Registration Code"
-      redirect_to root_path
     end
   end
-# end
 end
 
 private
 
   def reg_params
-    params.permit(:email, :unique_id)
+    params.require(:user).permit(:email, :first_name, :last_name, :home_phone, :cell_phone,
+                      :password, :password_confirmation )
   end
 end
