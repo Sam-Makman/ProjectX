7a0d275b2d3111659904d052358150148d7ad5d4
Tyler Moody <tmoody@lclark.edu>
2016-06-29 18:53:47 -0700

    db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,10 @@ class User < ActiveRecord::Base
      update_attribute(:remember_digest, User.digest(remember_token))
    end
 
+   def forget
+     update_attribute(:remember_digest, nil)
+   end
+   
    # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     return false if remember_digest.nil?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628192502) do
+ActiveRecord::Schema.define(version: 20160630014212) do
 
   create_table "caregivers", force: :cascade do |t|
     t.string   "first_name"
@@ -44,14 +44,17 @@ ActiveRecord::Schema.define(version: 20160628192502) do
   create_table "users", force: :cascade do |t|
     t.string   "device_id"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "home_phone"
     t.string   "cell_phone"
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["device_id"], name: "index_users_on_device_id", unique: true
