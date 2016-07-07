756bf4d47e1e783b5773a2d86123f32e95f68f8c
Sam <sammakman@gmail.com>
2016-07-07 13:10:36 -0700

    mailgun fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,5 +44,5 @@ Rails.application.configure do
   host = "localhost:3000"
   config.action_mailer.default_url_options = { host: host, protocol: 'https'}
 
-  Rails.application.routes.default_url_options[:host] = '???'
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
