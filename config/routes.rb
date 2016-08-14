75f0a2f9f376bb73a2515458cf25e1c0511ece49
Sam <sammakman@gmail.com>
2016-08-14 10:34:55 -0700

    made all api endpoints post

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
 root "users#new"
-get 'api/lookup'
-get 'api/service'
-get 'api/unregistered'
+post 'api/lookup'
+post 'api/service'
 get 'users/new'
 post 'users', to: "users#create"
 get 'users/edit/:id', to: 'users#edit', as: 'user'
-get 'api/regcode'
+post 'api/regcode'
 
 get    'login'   => 'sessions#new'
 post   'login'   => 'sessions#create'
